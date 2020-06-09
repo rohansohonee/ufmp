2276805aa1327973bd1effd925293cf0bedf79d1
Rohan Sohonee <rohansohonee@hotmail.com>
2020-06-09 13:42:14 +0530

    update param int to Duration

--- lib/utils/parseDuration.dart
+++ lib/utils/parseDuration.dart
@@ -1,14 +1,8 @@
-/// convert duration from seconds to milliseconds.
-int durationInMillis(int duration) {
-  return Duration(seconds: duration).inMilliseconds;
-}
-
 /// convert duration to human readable string.
-String prettyDuration(int duration) {
+String prettyDuration(Duration duration) {
   String seconds;
-  final _duration = Duration(seconds: duration);
-  final minutes = _duration.inMinutes.remainder(60);
-  final sec = _duration.inSeconds.remainder(60);
+  final minutes = duration.inMinutes.remainder(60);
+  final sec = duration.inSeconds.remainder(60);
   if (sec < 10)
     seconds = '0$sec';
   else
